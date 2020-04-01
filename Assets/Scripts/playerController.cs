426d186b2a3e77c1318528306aa47c90d0e75dda
danielmpadua <danielmarianopadua@gmail.com>
2020-03-31 23:19:38 -0300

    criado o scrip playerController

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+	private Rigidbody2D playerRB;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
